74c98d5058b26ec4896365889ab41e9b0aeb1a54
HerjunoNugroho <61776693+HerjunoNugroho@users.noreply.github.com>
2020-04-13 15:16:10 +0700

    Commit1

---
+++ Karyawan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2644
+{
+    class Karyawan
+    {
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public Karyawan(int nik, string nama, int bulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            if (bulanan < 0)
+            {
+                GajiBulanan = 0;
+            }
+            else
+            {
+                GajiBulanan = bulanan;
+            }
+        }
+
+        public void TampilkanKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * GajiBulanan;
+            GajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2644
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No NIK/NAMA          Gaji Bulanan");
+            Console.WriteLine("*****************************************");
+
+            Karyawan karyawan1 = new Karyawan(19112644, "Herjuno", 5000000);
+            karyawan1.TampilkanKaryawan();
+            
+            Karyawan karyawan2 = new Karyawan(19112000, "Nugroho", 3000000);
+            karyawan2.TampilkanKaryawan();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Asyiiikkk Kenaikan Gaji 10%");
+            Console.WriteLine("No NIK/NAMA          Gaji Bulanan");
+            Console.WriteLine("*****************************************");
+
+            karyawan1.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
